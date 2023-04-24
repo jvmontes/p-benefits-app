13de1d3eb3bd4fe02c1cc8ea127648557ad88953
Jorge Viramontes <>
2023-04-23 21:33:30 -0500

    Remove Dependents from the list, clean up calculation logic, remove calculate button and permeate changes from updates, add method to truncate names if longer than 10 chars, add number formatter.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { useState } from 'react'
 import AddPerson from '@/Components/AddPerson'
 import { v4 as uuid } from 'uuid';
-
-const inter = Inter({ subsets: ['latin'] })
+import { FaMinusSquare } from 'react-icons/fa'
 
 const DEFAULT_EMPLOYEE_COST = 1000;
 const DEFAULT_DEPENDENT_COST = 500;
@@ -17,42 +15,53 @@ export default function Home() {
   const [dependents, setDependents] = useState([]);
   const [totalCost, setTotalCost] = useState(0);
 
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
   function addEmployee(e) {
-    setEmployee(e);
+    var calculatedEmployeeCost = calculateDiscount(e, DEFAULT_EMPLOYEE_COST);
+    setEmployee({
+      firstName: e.firstName,
+      lastName: e.lastName,
+      cost: calculatedEmployeeCost
+    });
+
+    setTotalCost(0);
+    setTotalCost(prevTotalCost => prevTotalCost + calculatedEmployeeCost);
+    dependents.map((dependent) => {
+      setTotalCost(prevTotalCost => prevTotalCost + dependent.cost);
+    })
   }
 
   function addDependent(d) {
     const unique_id = uuid();
-    d.key = unique_id.slice(0, 8)
+    d.key = unique_id.slice(0, 8);
+    d.cost = calculateDiscount(d, DEFAULT_DEPENDENT_COST);
+
+    setTotalCost(prevTotalCost => prevTotalCost + d.cost);
     setDependents([...dependents, d]);
+  }
+
+  function onRemoveDependent(key) {
+    var dependentToRemove = dependents.find(dependent => dependent.key === key);
+    setTotalCost(prevTotalCost => prevTotalCost - dependentToRemove.cost);
+    setDependents(dependents.filter((dependent) => dependent.key !== key));
   }
 
   function calculateDiscount(person, cost) {
     return (person.firstName[0] === "A" || person.firstName[0] === "a") ? cost * DISCOUNT_MULTIPLE : cost;
   }
 
-  function calculateBenefits(event) {
-    event.preventDefault();
-
-    setTotalCost(0);
-
-    if (employee.firstName !== undefined) {
-      var calculatedEmployeeCost = calculateDiscount(employee, DEFAULT_EMPLOYEE_COST);
-      setEmployee({
-        firstName: employee.firstName,
-        lastName: employee.lastName,
-        cost: calculatedEmployeeCost
-      });
-      setTotalCost(prevTotalCost => prevTotalCost + calculatedEmployeeCost);
+  function truncateIfNecessary(string) {
+    if (string.length > 10) {
+      var truncated = string.slice(0, 9);
+      truncated = truncated + "..";
+      return truncated;
+    } else {
+      return string;
     }
-
-    setDependents(
-      dependents.map((dependent) => {
-        var calculatedDependentCost = calculateDiscount(dependent, DEFAULT_DEPENDENT_COST);
-        setTotalCost(prevTotalCost => prevTotalCost + calculatedDependentCost);
-        return { ...dependent, cost: calculatedDependentCost };
-      })
-    )
   }
 
   return (
@@ -78,21 +87,21 @@ export default function Home() {
               <AddPerson onAddPerson={addEmployee} actionButtonText="Add Employee"></AddPerson>
             </div>
 
-            <div>
-              <h2>Dependent Information</h2>
-              <p>Please use the following form to enter any optional dependents.</p>
-              <AddPerson onAddPerson={addDependent} actionButtonText="Add Dependent"></AddPerson>
-            </div>
-
+            {employee.firstName && (
+              <div>
+                <h2>Dependent Information</h2>
+                <p>Please use the following form to enter any optional dependents.</p>
+                <AddPerson onAddPerson={addDependent} actionButtonText="Add Dependent"></AddPerson>
+              </div>
+            )}
           </div>
 
           <div className={styles.content}>
             <h2>Benefits Cost</h2>
-
             {employee.firstName ?
               (
                 <div>
-                  {employee.cost > 0 && (<p>Total Cost: {totalCost}</p>)}
+                  <p>Total Cost: {formatter.format(totalCost)}</p>
                   <div>
                     <h3>Employee Info</h3>
                     <div className={styles.personCard}>
@@ -101,9 +110,9 @@ export default function Home() {
                     </div>
                     <div className={styles.personCard}>
                       <div>
-                        {employee.firstName} {employee.lastName}
+                        {truncateIfNecessary(employee.firstName)} {truncateIfNecessary(employee.lastName)}
                       </div>
-                      {employee.cost && (<div>{employee.cost}</div>)}
+                      {employee.cost && (<div>{formatter.format(employee.cost)}</div>)}
                     </div>
                   </div>
 
@@ -119,20 +128,17 @@ export default function Home() {
                         {dependents.map((dependent) => (
                           <div key={dependent.key} className={styles.personCard}>
                             <div>
-                              {dependent.firstName} {dependent.lastName}
+                              <FaMinusSquare style={{ color: 'red', cursor: 'pointer' }}
+                                onClick={() => onRemoveDependent(dependent.key)}></FaMinusSquare>
+                              {truncateIfNecessary(dependent.firstName)} {truncateIfNecessary(dependent.lastName)}
                             </div>
-                            {dependent.cost && (<div>{dependent.cost}</div>)}
+                            {dependent.cost && (<div>{formatter.format(dependent.cost)}</div>)}
                           </div>
 
                         ))}
                       </div>
                     )
                   }
-                  <form>
-                    <div>
-                      <button className={styles.button} onClick={calculateBenefits}>Calculate Benefits</button>
-                    </div>
-                  </form>
                 </div>) :
               (
                 <p>Benefits cost calculation will show here after employee info is entered.</p>
